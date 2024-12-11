188f4a6e6c89b320c9a298b1cf8f080d22e62b33
Youssef Ibrahim <youssefebrahimhosni@gmail.com>
2024-12-11 22:02:31 +0200

    Add project files.

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //single line comment
+            /*this is 
+              a multi 
+              line 
+              comment
+             */
+            //this is to print the string 
+            //using .NET 8 (long term support)
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SecondProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //second Project
+        }
+    }
+}
